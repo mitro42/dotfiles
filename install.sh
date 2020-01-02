3b70cc706cc3744b786803483d06098751455f0a
Attila Mitrocsak <attila.mitrocsak@gmail.com>
2020-01-02 17:31:20 +0100

    Added  gitconfig file with private email

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ ln -fs `pwd`/.tmux.conf ~/.tmux.conf
 ln -fs `pwd`/.prettierrc ~/.prettierrc
 ln -fs `pwd`/.clang-format ~/.clang-format
 ln -fs `pwd`/.gitconfig ~/.gitconfig
+ln -fs `pwd`/.gitconfig-priv ~/.gitconfig-priv
 VSCODE_CONFIG_DIR=~/.config/Code/User
 mkdir -p $VSCODE_CONFIG_DIR
 cp vscode/keybindings.json $VSCODE_CONFIG_DIR
